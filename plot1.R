3553d1c3eab1924860b73a8fee74a6336c3caf6d
robert lee <100LL@users.noreply.github.com>
2015-04-06 20:52:00 -0700

    - add datetime column so that it can be re-used - update formatting and comments

--- plot1.R
+++ plot1.R
@@ -2,28 +2,36 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
               "household_power_consumption.zip",
               "curl")
 unzip("household_power_consumption.zip")
-df <- read.table("household_power_consumption.txt",
-                 sep=";",
-                 na.strings="?",
-                 col.names=c("Date",
-                             "Time",
-                             "Global_active_power",
-                             "Global_reactive_power",
-                             "Voltage",
-                             "Global_intensity",
-                             "Sub_metering_1",
-                             "Sub_metering_2",
-                             "Sub_metering_3"),
-                 colClasses=c("character",
-                              "character",
-                              rep("numeric",7)),
-                 comment.char="",
-                 nrows=3000,
-                 #skip forward to the start of the data of interest
-                 skip=tail(grep("31/1/2007",readLines("household_power_consumption.txt")),1))
+df <- read.table(
+  "household_power_consumption.txt",
+  sep=";",
+  na.strings="?",
+  col.names=c("Date",
+              "Time",
+              "Global_active_power",
+              "Global_reactive_power",
+              "Voltage",
+              "Global_intensity",
+              "Sub_metering_1",
+              "Sub_metering_2",
+              "Sub_metering_3"),
+  colClasses=c("character",
+               "character",
+               rep("numeric",7)),
+  comment.char="",
+  nrows=3000,
+  # skip forward to the start of the data of interest
+  skip=tail(grep("31/1/2007",
+                 readLines("household_power_consumption.txt")),1)
+)
 
+# convert date column from character class to date class
 df[, "Date"] <- as.Date(df[, "Date"],"%d/%m/%Y")
+# remove rows outside of relevant date range
 df <- subset(df,Date < "2007-02-03")
+# create a new column dt that combines the date and time elements
+dt <- strptime(paste(df$Date,df$Time),"%Y-%m-%d %T")
+df <- cbind(df,dt)
 
 png("plot1.png")
 hist(df$Global_active_power,
--- plot4.R
+++ plot4.R
@@ -2,65 +2,76 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
               "household_power_consumption.zip",
               "curl")
 unzip("household_power_consumption.zip")
-df <- read.table("household_power_consumption.txt",
-                 sep=";",
-                 na.strings="?",
-                 col.names=c("Date",
-                             "Time",
-                             "Global_active_power",
-                             "Global_reactive_power",
-                             "Voltage",
-                             "Global_intensity",
-                             "Sub_metering_1",
-                             "Sub_metering_2",
-                             "Sub_metering_3"),
-                 colClasses=c("character",
-                              "character",
-                              rep("numeric",7)),
-                 comment.char="",
-                 nrows=3000,
-                 #skip forward to the start of the data of interest
-                 skip=tail(grep("31/1/2007", readLines("household_power_consumption.txt")),1))
+df <- read.table(
+  "household_power_consumption.txt",
+  sep=";",
+  na.strings="?",
+  col.names=c("Date",
+              "Time",
+              "Global_active_power",
+              "Global_reactive_power",
+              "Voltage",
+              "Global_intensity",
+              "Sub_metering_1",
+              "Sub_metering_2",
+              "Sub_metering_3"),
+  colClasses=c("character",
+               "character",
+               rep("numeric",7)),
+  comment.char="",
+  nrows=3000,
+  # skip forward to the start of the data of interest
+  skip=tail(grep("31/1/2007",
+                 readLines("household_power_consumption.txt")),1)
+)
 
+# convert date column from character class to date class
 df[, "Date"] <- as.Date(df[, "Date"],"%d/%m/%Y")
+# remove rows outside of relevant date range
 df <- subset(df,Date < "2007-02-03")
+# create a new column dt that combines the date and time elements
+dt <- strptime(paste(df$Date,df$Time),"%Y-%m-%d %T")
+df <- cbind(df,dt)
 
 png("plot4.png")
 par(mfrow=c(2,2))
 with(df, {
-  plot(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
-       df$Global_active_power,
+  plot(dt,
+       Global_active_power,
        type="l",
        ylab="Global Active Power",
        xlab="")
-  plot(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
+  plot(dt,
        Voltage,
        type="l",
        ylab="Voltage",
        xlab="datetime")
-  plot(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
-       df$Sub_metering_1,
+  plot(dt,
+       Sub_metering_1,
        type="l",
        ylab="Energy sub metering",
        xlab="")
-  lines(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
-        df$Sub_metering_2,
+  lines(dt,
+        Sub_metering_2,
         type="l",
         xlab="",
         col="red")
-  lines(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
-        df$Sub_metering_3,
+  lines(dt,
+        Sub_metering_3,
         type="l",
         xlab="",
         col="blue")
   legend("topright",
          col=c("black","red","blue"),
-         legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         legend=c("Sub_metering_1",
+                  "Sub_metering_2",
+                  "Sub_metering_3"),
          lty=1,
          lwd=1,
          bty="n")
-  plot(strptime(paste(df$Date,df$Time),"%Y-%m-%d %T"),
+  plot(dt,
        Global_reactive_power,
        type="l",
-       xlab="datetime")})
+       xlab="datetime")
+})
 dev.off()
